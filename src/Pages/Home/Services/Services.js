039aa3bfca3050ea75b78bf05d4e112919db1fb1
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-04-10 22:49:39 +0600

    services and service api data readering with css styled

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,12 +1,23 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
+import './Services.css';
 const Services = () => {
     
-
-    
+/* load data from json file  */
+const [services,setservices] = useState([]);
+useEffect(()=>{
+    fetch('services.json')
+    .then(res=>res.json())
+    .then(data=>setservices(data)); // data loaded
+},[services])
     return (
         <div>
-            <h2>this is services</h2>
+        <h2 className='services-tilte'>our services : {services.length}</h2>
+        <div className='services-container'>
+        {
+            services.map(service => <Service key={service.id} Service={service}></Service>)
+        }
+        </div>
         </div>
     );
 };
